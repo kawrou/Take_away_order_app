8d1bfc16fa91ff4a886277de1689a351f4abb6f5
Alan Gardiner <kwarouag@gmail.com>
2023-11-24 21:26:33 +0000

    Designed some integration tests for TDD

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -2,6 +2,7 @@ from lib.food_item import*
 from lib.menu import*
 from lib.order import*
 
+
 """
 Given some food to be added to the menu
 Instances of food should be added to the menu_items list if it's not already in the list
@@ -9,10 +10,17 @@ show_menu should then display all the food on the menu if it's availble to order
 """
 def test_view_menu():
     #new_menu = Menu()
+    #FoodItem("Pizza", £5) => name: "Pizza" price: £5
     #add_food(food_1) & add_food(food_2) gets added to menu_items list
     #show_menu() => [food_1, food_2]
-    pass
-
+    new_menu = Menu()
+    pizza = FoodItem("Pizza", 5)
+    hot_dog = FoodItem("Hot Dog", 3)
+    burger = FoodItem("Burger", 4)
+    new_menu.add_food(pizza)
+    new_menu.add_food(hot_dog)
+    new_menu.add_food(burger)
+    assert new_menu.show_menu() == [pizza, hot_dog, burger]
 """
 Given a menu list filled with instances of food items
 show_menu should then display all the food on the menu that is available to order
